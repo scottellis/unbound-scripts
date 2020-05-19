85a09576421ec418b5be5bd3f10c9cb2acdb124a
Scott Ellis <scott@jumpnowtek.com>
2020-05-19 08:52:38 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,57 @@
+### unbound-scripts
+
+A script to fetch, parse and consolidate advertising and malware blocklists and format for use in [unbound(8)][unbound].
+
+The blocklists tend to come in one of two formats:
+
+* host-format: 127.0.0.0 aaa.com or 0.0.0.0 bbb.com
+* simple-format: aaa.com
+
+There are three url lists in the script.
+
+Place new urls in the appropriate list so they are correctly handled.
+
+The script output gets saved to */tmp/blocklist*.
+
+The output format is
+
+    local-zone: "aaa.com" always_nxdomain
+
+for each entry in the blocklist.
+
+A response type of *static* can be used instead of *always_nxdomain*.
+See the *local-zone* section of [unbound.conf(5)][unbound-conf] for details.
+
+If satisfied with the result, copy the list to a more permanent location. I use */var/unbound/etc/blocklist.conf*. 
+
+Then tell [unbound][unbound] to use the list with an [unbound.conf(5)][unbound-conf] include statement.
+
+    server:
+        ...
+        include: /var/unbound/etc/blocklist.conf
+        ...
+
+To monitor effectiveness, enable *extended-statistics* in [unbound.conf(5)][unbound.conf].
+
+    server:
+        ...
+        extended-statistics: yes
+        ...
+
+Then [unbound-control(8)][unbound-control] with a *stats* or *stats_noreset* argument can be used to monitor blocked queries.
+
+    root@nuc:~# unbound-control stats_noreset | grep num.answer
+    num.answer.rcode.NOERROR=180
+    num.answer.rcode.FORMERR=0
+    num.answer.rcode.SERVFAIL=0
+    num.answer.rcode.NXDOMAIN=1078
+    num.answer.rcode.NOTIMPL=0
+    num.answer.rcode.REFUSED=0
+    num.answer.rcode.nodata=16
+    num.answer.secure=0
+    num.answer.bogus=0
+
+  
+[unbound]: https://man.openbsd.org/unbound
+[unbound-conf]: https://man.openbsd.org/unbound.conf
+[unbound-control]: https://man.openbsd.org/unbound-control 
---
+++ blocklist.sh
@@ -0,0 +1,101 @@
+#!/bin/sh
+#
+# Run with 'verbose' as an argument to see progress
+#
+
+tmpfile=$(mktemp)
+
+blocklist="/tmp/blocklist.conf"
+
+verbose=${1:-silent}
+
+#
+# List of dns-over-https servers
+#
+doh_servers="\
+    http://octo.jumpnow/downloads/doh_servers.txt \
+" 
+
+#
+# For lists in /etc/hosts format like this
+# 127.0.0.1 aaa.com 
+# 0.0.0.0   bbb.com 
+# 
+hosts_format="\
+    http://sysctl.org/cameleon/hosts \
+    https://adaway.org/hosts.txt \
+    https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts \
+    https://raw.githubusercontent.com/BartsPrivacy/PrivacyHostList/master/BlockHosts-Facebook.txt \
+    https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt \
+"
+
+#
+# For simple lists with just the domain name 
+# 
+simple_format="\
+    https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt \
+    https://s3.amazonaws.com/lists.disconnect.me/simple_tracking.txt \
+    https://mirror1.malwaredomains.com/files/justdomains \
+    https://v.firebog.net/hosts/Easyprivacy.txt \
+    https://v.firebog.net/hosts/Prigent-Ads.txt \
+    https://gitlab.com/quidsup/notrack-blocklists/raw/master/notrack-blocklist.txt \
+    https://s3.amazonaws.com/lists.disconnect.me/simple_malvertising.txt \
+    https://v.firebog.net/hosts/AdguardDNS.txt \
+"
+
+#
+# Fetch and strip comments, blank lines and trailing white space
+#
+function fetch_and_strip {
+  ftp -VMo - $1 | \
+      sed -e 's/#.*$//' -e '/^[[:space:]]*$/d' -e 's/[[:space:]]*$//'
+}
+
+#
+# Add canary domain to block DNS over HTTPS (DOH)
+#
+echo 'use-application-dns.net' >> $tmpfile
+
+for list in $doh_servers; do
+    [ $verbose == "verbose" ] && echo "Fetching $list"
+    fetch_and_strip $list >> $tmpfile
+done
+
+
+for list in $simple_format; do
+    [ $verbose == "verbose" ] && echo "Fetching $list"
+    fetch_and_strip $list >> $tmpfile
+done
+
+#
+# Ugly special cases..., need a whitelist
+#
+for list in $hosts_format; do
+    [ $verbose == "verbose" ] && echo "Fetching $list"
+    fetch_and_strip $list \
+        | egrep '^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' \
+        | awk '{ print $2 }' \
+        | egrep -v '^broadcasthost$' \
+        | egrep -v '^local$' \
+        | egrep -v '^localhost\.localdomain$' \
+        | egrep -v '^0\.0\.0\.0$' \
+        >> $tmpfile
+done
+
+
+#
+# Sort, remove duplicates, then output in unbound.conf format
+#
+# Possible response types can be found in unbound.conf(5) in the local-zone
+# section. Some options are 'static', 'always_nxdomain', 'refuse', 'deny'
+#
+sort -fu $tmpfile \
+    | awk '{ print "local-zone: \"" $1 "\" always_nxdomain" }' \
+    > $blocklist
+
+
+rm -f $tmpfile
+
+[ $verbose == "verbose" ] && echo "Wrote file: $blocklist"
+
+exit 0
