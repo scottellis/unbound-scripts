0869f1e46a0d313fcb48e6b4ce47d5cf08c4e053
Scott Ellis <scott@jumpnowtek.com>
2020-05-24 05:31:47 -0400

    Add more detail to blockstats.pl

--- blockstats.pl
+++ blockstats.pl
@@ -1,10 +1,18 @@
 #!/usr/bin/perl -w
+#
+# %stats is a hash of hashes with the following structure
+#
+# $stats{host_ip or all}{ 'total' => <count>,
+#                         'success' => <count>,
+#                         'blocked' => <count>,
+#                         'fail' => <count> }
 
 my $log = "/var/log/unbound";
 
 my %noerror;
 my %nxdomain;
 my %servfail;
+my %stats = ( total => { queries => 0, success => 0, block => 0, fail => 0 } );
 
 open(FILE, $log) || die "Could not open $log\n";
 
@@ -23,40 +31,60 @@ LINE: while (<FILE>) {
     # this line can be removed after that change
     next LINE if ($fields[8] =~ /noscript-csp.invalid/);
 
-    # skip my local domain 
+    # skip my local domain
     next LINE if ($fields[8] =~ /jumpnow.$/);
 
     # skip Chrome's non-existent domain requests (no '.' in hostname)
     next LINE if ($fields[8] !~ /\.\w+/);
 
-    my $host = $fields[8];
-    $host =~ s/\.$//;
+    $stats{'total'}{'queries'}++;
+
+    my $src = $fields[7];
+
+    if (exists($stats{$src})) {
+        $stats{$src}{'queries'}++;
+    }
+    else {
+        $stats{$src} = { queries => 1, success => 0, block => 0, fail => 0 };
+    }
+
+    my $dst = $fields[8];
+    $dst =~ s/\.$//;
 
     my $response = $fields[11];
 
     if ($response eq 'NOERROR') {
-        if (exists($noerror{$host})) {
-            $noerror{$host}++;
+        if (exists($noerror{$dst})) {
+            $noerror{$dst}++;
         }
         else {
-            $noerror{$host} = 1;
+            $noerror{$dst} = 1;
         }
+
+        $stats{'total'}{'success'}++;
+        $stats{$src}{'success'}++;
     }
     elsif ($response eq 'NXDOMAIN') {
-        if (exists($nxdomain{$host})) {
-            $nxdomain{$host}++;
+        if (exists($nxdomain{$dst})) {
+            $nxdomain{$dst}++;
         }
         else {
-            $nxdomain{$host} = 1;
+            $nxdomain{$dst} = 1;
         }
+
+        $stats{'total'}{'block'}++;
+        $stats{$src}{'block'}++;
     }
     elsif ($response eq 'SERVFAIL') {
-        if (exists($servfail{$host})) {
-            $servfail{$host}++;
+        if (exists($servfail{$dst})) {
+            $servfail{$dst}++;
         }
         else {
-            $servfail{$host} = 1;
+            $servfail{$dst} = 1;
         }
+
+        $stats{'total'}{'fail'}++;
+        $stats{$src}{'fail'}++;
     }
     else {
         print("Unhandled response type: $response\n");
@@ -65,38 +93,37 @@ LINE: while (<FILE>) {
 
 close(FILE);
 
-my $success = 0;
-my $blocked = 0;
-my $failed = 0;
+print("\n==== Host Query Summaries ====\n");
 
-foreach my $host (keys %noerror) {
-    $success += $noerror{$host};
+foreach my $host (sort keys %stats) {
+    my $queries = $stats{$host}{'queries'};
+    my $success = $stats{$host}{'success'};
+    my $block = $stats{$host}{'block'};
+    my $fail = $stats{$host}{'fail'};
+
+    print("\n$host\n");
+    printf("    Queries: %d\n", $queries);
+    printf("    Success: %d (%.1f%%)\n", $success, (100.0 * $success) / $queries);
+    printf("    Blocked: %d (%.1f%%)\n", $block, (100.0 * $block) / $queries);
+    printf("     Failed: %d (%.1f%%)\n", $fail, (100.0 * $fail) / $queries);
 }
 
-foreach my $host (keys %nxdomain) {
-    $blocked += $nxdomain{$host};
-}
+print("\n==== Blocked Targets ====\n\n");
 
-foreach my $host (keys %servfail) {
-    $failed += $servfail{$host};
-}
-
-my $total = $success + $blocked + $failed;
-
-print("\n==== Query Summary ====\n");
-
-print("    Total: $total\n");
-printf("  Success: %d (%.1f%%)\n", $success, (100.0 * $success) / $total);
-printf("  Blocked: %d (%.1f%%)\n", $blocked, (100.0 * $blocked) / $total);
-printf("   Failed: %d (%.1f%%)\n", $failed, (100.0 * $failed) / $total);
-
-print("\n==== Blocked Hosts ====\n");
-
-# sort blocked hosts in count descending order
+# sort blocked targets in count descending order
 my @sorted_hosts = sort { $nxdomain{$b} <=> $nxdomain{$a} } keys %nxdomain;
 
 foreach my $host (@sorted_hosts) {
     print("$host : $nxdomain{$host}\n");
+}
+
+print("\n==== Failed Targets ====\n\n");
+
+# sort failed targets in count descending order
+@sorted_hosts = sort { $servfail{$b} <=> $servfail{$a} } keys %servfail;
+
+foreach my $host (@sorted_hosts) {
+    print("$host : $servfail{$host}\n");
 }
 
 print("\n");
